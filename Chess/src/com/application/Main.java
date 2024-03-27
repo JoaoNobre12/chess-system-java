87311abe45cf2a7d7114e605a82b3c3ad198ffea
joao nobre <joaovitornobre12@hotmail.com>
2024-03-27 00:24:33 -0300

    second commit

--- Chess/src/com/application/Main.java
+++ Chess/src/com/application/Main.java
@@ -1,7 +1,11 @@
 package com.application;
 
+import com.boardgame.Position;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("ok");
+		Position pos = new Position(3, 6);
+		
+		System.out.println(pos);
 	}
 }
