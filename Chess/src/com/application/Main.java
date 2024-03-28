26f064070720b05f4867e839c1b485ba4d10d8cd
joao nobre <joaovitornobre12@hotmail.com>
2024-03-28 00:50:35 -0300

    muda para intellij

--- Chess/src/com/application/Main.java
+++ Chess/src/com/application/Main.java
@@ -1,10 +1,12 @@
 package com.application;
 
+import com.boardgame.Board;
 import com.boardgame.Position;
 
 public class Main {
 	public static void main(String[] args) {
 		Position pos = new Position(3, 6);
+		Board board = new Board(8, 8);
 		
 		System.out.println(pos);
 	}
---
+++ Chess/src/com/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package com.boardgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		this.position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+}
