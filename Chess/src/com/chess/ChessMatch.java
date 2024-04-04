454ccb7b257fc20703d356abbd988a8d61126767
joao nobre <joaovitornobre12@hotmail.com>
2024-04-04 01:20:51 -0300

    creates chessposition and prints pieces with color

--- Chess/src/com/chess/ChessMatch.java
+++ Chess/src/com/chess/ChessMatch.java
@@ -27,9 +27,23 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece,new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,1));
-        board.placePiece(new King(board, Color.WHITE), new Position(0,4));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 
     }
 }
---
+++ Chess/src/com/chess/ChessPosition.java
@@ -0,0 +1,36 @@
+package com.chess;
+
+import com.boardgame.Position;
+
+public class ChessPosition {
+    private int row;
+    private char column;
+
+    public ChessPosition(char column, int row) {
+        if(column < 'a' || column > 'h' || row < 1 || row > 8)
+            throw new ChessException("Error while instantiating ChessPosition: a1 to h8 only.");
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), position.getRow() - 8);
+    }
+
+    @Override
+    public String toString(){
+        return "" + this.column + this.row;
+    }
+}
